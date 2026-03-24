abd925e0fece2a375a10590b73c889b69e992539
Hasan Haj Hasan <hasanhajhasan98@gmail.com>
2026-03-24 11:58:18 +0200

    fix: add "Payroll Entry" to ignore_links_on_delete list

--- hrms/hooks.py
+++ hrms/hooks.py
@@ -346,7 +346,7 @@ override_doctype_dashboards = {
 #
 # auto_cancel_exempted_doctypes = ["Auto Repeat"]
 
-ignore_links_on_delete = ["PWA Notification"]
+ignore_links_on_delete = ["PWA Notification","Payroll Entry"]
 
 # User Data Protection
 # --------------------
